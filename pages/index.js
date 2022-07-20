b29072b94ecb2c3d16c2e5a266fd5da3208b21ec
Fernando Miguel <fjmiguel.dev@gmail.com>
2022-07-20 10:50:04 +0200

    implementing basic index.js page

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import styles from "../styles/Home.module.css";
+
+const App = () => {
+  return (
+    <div className={styles.wrapper}>
+      <p>Weather App wrapper</p>
+    </div>
+  );
+};
+
+export default App;
